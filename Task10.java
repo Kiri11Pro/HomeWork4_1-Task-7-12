1f7d04d00046c687fb29c8d3411d14756ee55cf2
Kiri11Pro <kirill11che@gmail.com>
2024-08-31 21:04:35 +0700

    fix Task10,7

--- Task10.java
+++ Task10.java
@@ -1,29 +1,34 @@
 public class Task10 {
     public static void main(String[] args) {
-//        int a,b;
-//        a=1;
-//        for (int i = 2; i <= 20; i++) {
-//            boolean isPrime = true;
-//            for (int j = 2; j <= Math.sqrt(i); j++) {
-//                if ((i % j) == 0) {
-//                    isPrime = false;
-//                    break;
-//                }
-//            }
-//            if (isPrime) {
-//                a*=i;
-//            }
-//        }
-//        b=a;
-//        System.out.println(a);
-//        for (int j = 1; j<=20 ; j++){
-//            if (b%j!=0){
-//                b=b+a;
-//                j--;
-//            }
-//
-//        }System.out.println(b);
-
+        int a, b, k, c;
+        c = 0;
+        a = 1;
+        for (int i = 2; i <= 20; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if ((i % j) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                a *= i;
+            }
+        }
+        b = a;
+        for (int j = 1; j <= 20; j++) {
+            if (b % j != 0) {
+                for (k = 2; k * k <= j; k++) {
+                    if (j % k == 0) {
+                        c = k;
+                        break;
+                    }
+                }
+                b = b * c;
+                j--;
+            }
+        }
+        System.out.println(b);
     }
 
 }
--- Task7.java
+++ Task7.java
@@ -1,29 +1,42 @@
 public class Task7 {
+//    public class BitPresentation {
+//        private static int number = 17;
+//        public static void main(String[] args) {
+//            int module = (int) Math.pow(2, 30);
+//            while(module > number) {
+//                module>>=1;
+//            }
+//            System.out.print("The Number " + number + " in bit presentation: ");
+//            while (module > 0) {
+//                System.out.print(number/module > 0 ? 1 : 0);
+//                number%=module;
+//                module>>=1;
+//            }
+//        }
+//    }
     public static void main(String[] args) {
-            int a,b,j,i, max,noti;
-            i=4;
-//            String g = Integer.toBinaryString(i,4);
-//            i= ~i;
-//            String h = Integer.toBinaryString(i);
-//            max = Integer.MAX_VALUE;
-//            i=(i^max);
-//            max = i^max;
-//            i=i^max;
-        j=0;
-            while (i!=0){
-                j++;
-                if (i%2 == 0){
-                    b=0;
-                    System.out.print(b);
-                }
-                else {
-                    a=1;
-                    System.out.print(a);
-                }
+            int j,i;
+            i=5;
+        j=(int) Math.pow(2,30);
+        while (j>i) {
+            j >>= 1;
+        }
+        while (j>0) {
+            System.out.print(i / j > 0 ? 1 : 0);
+            i %= j;
+            j >>= 1;
+        }
+//        while (i!=0 ){
+//                j++;
+//                if (i%2 != 0) {
+//                    a=(int)(Math.pow(10,j));
+//                    System.out.print(a);
+//                }
 //                c= "a"+b;
-                i=i/2;
+//                i=i/2;
 //                System.out.println(c);
             }
 
+
     }
-}
+
